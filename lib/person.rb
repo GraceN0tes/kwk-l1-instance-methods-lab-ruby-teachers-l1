50e458126a5c7bb1046a4d05ce2813270bbc2cfa
Grace Park <parkgra3@gmail.com>
2018-06-25 14:54:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,20 @@
 class Person
   def talk
+    @eye_color = eye_color
+    @hair_color = hair_color
+    @height = height
+    @weight = weight
+    
+  # puts "I love my new #{hair_color}, it matches with my #{eye_color}"
     puts "Hello World!"
   end
 
   def walk
     puts "The Person is walking"
   end
-end
+end''#adele_goldberg 'alan_kay'
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
+
